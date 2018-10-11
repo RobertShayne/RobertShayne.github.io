feaadea18d5904f9e8fa79403b98a5b1b2f6aff3
RobertShayne <realshayne@gmail.com>
2018-10-11 15:47:24 +0800

    1

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var statusText = document.querySelector('#statusText');
 
 statusText.addEventListener('click',function(){
-	//statusText.textContent = "clicked";
 	statusText.textContent = BLEscanner.connect();
 });
 
