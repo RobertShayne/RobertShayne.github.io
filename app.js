3d32a09ec1f64e92695bf924b8b3785890a679b1
RobertShayne <realshayne@gmail.com>
2018-10-11 14:36:50 +0800

    1

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ var statusText = document.querySelector('#statusText');
 statusText.addEventListener('click',function(){
 	statusText.textContent = "clicked";
 });
+
+window.onload = function() {
+  statusText.textContent = "Click me now!!";
+};
